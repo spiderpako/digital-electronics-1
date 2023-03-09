3bbc361db1733d214760ce6d106a9a3aa94bbac7
spiderpako <240198@vutbr.cz>
2023-03-09 12:23:57 +0100

    Cviceni 5

---
+++ 05-ffs/flip_flops/flip_flops.srcs/sources_1/new/d_ff_rst.vhd
@@ -0,0 +1,65 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/09/2023 11:13:55 AM
+-- Design Name: 
+-- Module Name: d_ff_rst - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity d_ff_rst is
+    Port ( clk : in STD_LOGIC; -- main clock
+           rst : in STD_LOGIC; -- high-active synchronous reset
+           d : in STD_LOGIC; -- data input
+           q : out STD_LOGIC; -- main output
+           q_bar : out STD_LOGIC); -- complementary output
+end d_ff_rst;
+
+architecture Behavioral of d_ff_rst is
+
+begin
+    --------------------------------------------------------
+    -- p_d_ff_rst:
+    -- D type flip-flop with a high-active sync reset and
+    -- rising-edge clk.
+    -- q(n+1) = d
+    --------------------------------------------------------
+    p_d_ff_rst : process (clk) is
+    begin
+        if rising_edge(clk) then  -- Synchronous process
+            if rst = '1' then 
+                q <= '0'; -- high-active reset
+                q_bar <= '1';
+        
+        else
+
+                q     <= d;
+                q_bar <= not d;
+        end if;
+        end if;
+    end process p_d_ff_rst;
+end architecture behavioral;
